9bcaa264a38b9327d079288d4afff7606f75cdbf
skatpgusskat <skatpgusskat@naver.com>
2017-06-29 23:02:05 +0900

    Test rendering

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: ['./lib/parse.js'],
+    app: ['./lib/index.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
